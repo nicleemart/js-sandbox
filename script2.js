ba763085e6ca5850ef4a91129c1364835312c5f6
Nikki <nicleemart@gmail.com>
2016-10-17 20:35:37 -0500

    changes all to inactive

--- script2.js
+++ script2.js
@@ -1,21 +1,17 @@
 window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
-    var activeElem = document.getElementsByClassName("tabs__tab--active");
-    var inactiveElem = document.getElementsByClassName("tabs__tab--inactive");
 
     for (i = 0; i < tab.length; i++) {
         tab[i].addEventListener("click", changeActiveState.bind(null, i));
-        // Loop through like class when clicked, use bind to attach function and param to click event
     };
 
     function changeActiveState(index) {
-        // Use index variable to indicate position in array
 
-        if (tab[index].className === "tabs__tab--inactive") {
-            tab[index].className = "tabs__tab--active";
+        if (tab[index].className === "tabs__tab tabs__tab--inactive") {
+            tab[index].className = "tabs__tab tabs__tab--active";
 
         } else {
-            tab[index].className = "tabs__tab--inactive";
+            tab[index].className = "tabs__tab tabs__tab--inactive";
         }
     }
 
