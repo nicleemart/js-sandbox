79986e43f347472706bb34fca41302d08d4b1f35
Nikki <nicleemart@gmail.com>
2016-10-18 10:47:51 -0500

    working on displaying content when tab is clicked on

--- script2.js
+++ script2.js
@@ -1,18 +1,27 @@
 window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
     var active = document.getElementsByClassName("tabs__tab--active");
+    var content = document.getElementsByClassName("js-content");
 
     for (i = 0; i < tab.length; i++) {
         tab[i].addEventListener("click", changeActiveState.bind(null, i));
     };
 
     function changeActiveState(index) {
-
-    for (x = 0; x < tab.length; x++) {
-    	tab[x].classList.remove("tabs__tab--active");
-    };
-    	tab[index].classList.add("tabs__tab--active");
-
+        for (x = 0; x < tab.length; x++) {
+            tab[x].classList.remove("tabs__tab--active");
+        };
+        tab[index].classList.add("tabs__tab--active");
     }
+
+    var goToTab = function(index) {
+        if (index !== activeIndex && index >= 0 && index <= tab.length) {
+            tab[activeIndex].classList.remove("tabs__tab--active");
+            tab[index].classList.add("tabs__tab--active");
+            content[activeIndex].classList.remove("js-content--active");
+            content[index].classList.add("js-content--active");
+            activeIndex = index;
+        }
+    };
 
 });
