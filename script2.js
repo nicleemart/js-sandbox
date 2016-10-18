eaf5ed8e922363f60bb86cfa0dc9e09c27b5a092
Nikki <nicleemart@gmail.com>
2016-10-18 09:41:33 -0500

    shortened code

--- script2.js
+++ script2.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
-    var inactive = document.getElementsByClassName("tabs__tab--inactive");
     var active = document.getElementsByClassName("tabs__tab--active");
 
     for (i = 0; i < tab.length; i++) {
@@ -9,12 +8,8 @@ window.addEventListener("load", function() {
 
     function changeActiveState(index) {
 
-        if (inactive[index].className === "tabs__tab tabs__tab--inactive js-tabs") {
-            inactive[index].className = "tabs__tab tabs__tab--active js-tabs";
+    	tab[index].classList.add("tabs__tab--active");
 
-        } else {
-            inactive[index].className = "tabs__tab tabs__tab--active js-tabs";
-        }
     }
 
 });
