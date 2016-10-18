6775061c14bfa70a9dc14d64fd10573019a39db5
Nikki <nicleemart@gmail.com>
2016-10-18 14:57:12 -0500

    commented out code

--- script2.js
+++ script2.js
@@ -2,13 +2,13 @@ window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
     var active = document.getElementsByClassName("tabs__tab--active");
     var content = document.getElementsByClassName("js-content");
-    var index = 1;
-    var activeIndex = 0;
+    // var index = 1;
+    // var activeIndex = 0;
 
     for (i = 0; i < tab.length; i++) {
         tab[i].addEventListener("click", function(){
          changeActiveState.bind(null, i);
-         goToTab();
+         // goToTab();
     	});
     };
 
@@ -19,12 +19,12 @@ window.addEventListener("load", function() {
         tab[index].classList.add("tabs__tab--active");
     };
 
-    function goToTab() {
-        if (index !== activeIndex && index >= 0 && index <= tab.length) {
-            content[activeIndex].classList.remove("js-content--active");
-            content[index].classList.add("js-content--active");
-            activeIndex = index;
-        }
-    };
+    // function goToTab() {
+    //     if (index !== activeIndex && index >= 0 && index <= tab.length) {
+    //         content[activeIndex].classList.remove("js-content--active");
+    //         content[index].classList.add("js-content--active");
+    //         activeIndex = index;
+    //     }
+    // };
 
 });
