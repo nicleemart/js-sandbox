255f8e76d4553d14385558657571807d84b37b78
Nikki <nicleemart@gmail.com>
2016-10-17 20:02:14 -0500

    keeps class instead of removing

--- script2.js
+++ script2.js
@@ -1,21 +1,22 @@
 window.addEventListener("load", function() {
-var tab = document.querySelector(".js-tabs");
+    var tab = document.getElementsByClassName("js-tabs");
+    var activeElem = document.getElementsByClassName("tabs__tab--active");
+    var inactiveElem = document.getElementsByClassName("tabs__tab--inactive");
 
-    function changeTabClass() {
+    for (i = 0; i < tab.length; i++) {
+        tab[i].addEventListener("click", changeActiveState.bind(null, i));
+        // Loop through like class when clicked, use bind to attach function and param to click event
+    };
 
-        if (tab.contains("tabs__tab--inactive")) {
+    function changeActiveState(index) {
+        // Use index variable to indicate position in array
 
-            tab.remove("tabs__tab--inactive");
+        if (tab[index].className === "tabs__tab--inactive") {
+            tab[index].className = "tabs__tab--active";
 
         } else {
-
-            tab.add("tabs__tab--active");
-
+            tab[index].className = "tabs__tab--inactive";
         }
-    } 
-    {
-        for (i = 0; i < tab.length; i++) {
-            tab[i].addEventListener("click", changeTabClass);
-        };
     }
+
 });
