806bfae4e5df6c6e4987e4801c5c1b387e67aa8b
Nikki <nicleemart@gmail.com>
2016-10-18 14:51:46 -0500

    wip

--- script2.js
+++ script2.js
@@ -7,12 +7,12 @@ window.addEventListener("load", function() {
 
     for (i = 0; i < tab.length; i++) {
         tab[i].addEventListener("click", function(){
-         changeActiveState();
+         changeActiveState.bind(null, i);
          goToTab();
     	});
     };
 
-    function changeActiveState() {
+    function changeActiveState(index) {
         for (x = 0; x < tab.length; x++) {
             tab[x].classList.remove("tabs__tab--active");
         };
@@ -21,8 +21,6 @@ window.addEventListener("load", function() {
 
     function goToTab() {
         if (index !== activeIndex && index >= 0 && index <= tab.length) {
-            tab[activeIndex].classList.remove("tabs__tab--active");
-            tab[index].classList.add("tabs__tab--active");
             content[activeIndex].classList.remove("js-content--active");
             content[index].classList.add("js-content--active");
             activeIndex = index;
