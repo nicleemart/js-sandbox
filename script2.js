3e781d2b4c0ca9aeca6df0426e89e9ede85838b7
Nikki <nicleemart@gmail.com>
2016-10-18 11:13:31 -0500

    code to display content from tabs does not work

--- script2.js
+++ script2.js
@@ -1,18 +1,31 @@
 window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
     var active = document.getElementsByClassName("tabs__tab--active");
+    var content = document.getElementsByClassName("js-content");
+    var activeIndex = 0;
 
     for (i = 0; i < tab.length; i++) {
-        tab[i].addEventListener("click", changeActiveState.bind(null, i));
+        tab[i].addEventListener("click", function(){
+         changeActiveState();
+         goToTab();
+    	});
     };
 
     function changeActiveState(index) {
-
-    for (x = 0; x < tab.length; x++) {
-    	tab[x].classList.remove("tabs__tab--active");
+        for (x = 0; x < tab.length; x++) {
+            tab[x].classList.remove("tabs__tab--active");
+        };
+        tab[index].classList.add("tabs__tab--active");
     };
-    	tab[index].classList.add("tabs__tab--active");
 
-    }
+    var goToTab = function(index) {
+        if (index !== activeIndex && index >= 0 && index <= tab.length) {
+            tab[activeIndex].classList.remove("tabs__tab--active");
+            tab[index].classList.add("tabs__tab--active");
+            content[activeIndex].classList.remove("js-content--active");
+            content[index].classList.add("js-content--active");
+            activeIndex = index;
+        }
+    };
 
 });
