ded5e059d9a94dbcf8d404144db2028fdbc6705b
Nikki <nicleemart@gmail.com>
2016-10-18 13:52:48 -0500

    hard coded values work

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
     var tab = document.getElementsByClassName("js-tabs");
     var active = document.getElementsByClassName("tabs__tab--active");
     var content = document.getElementsByClassName("js-content");
-    var index = 0;
+    var index = 1;
     var activeIndex = 0;
 
     for (i = 0; i < tab.length; i++) {
@@ -12,14 +12,14 @@ window.addEventListener("load", function() {
     	});
     };
 
-    function changeActiveState(index) {
+    function changeActiveState() {
         for (x = 0; x < tab.length; x++) {
             tab[x].classList.remove("tabs__tab--active");
         };
         tab[index].classList.add("tabs__tab--active");
     };
 
-    function goToTab(index) {
+    function goToTab() {
         if (index !== activeIndex && index >= 0 && index <= tab.length) {
             tab[activeIndex].classList.remove("tabs__tab--active");
             tab[index].classList.add("tabs__tab--active");
