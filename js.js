50ee5f2d608e071496f5ca62ab02188ba0fd30a1
Nikki <nicleemart@gmail.com>
2016-10-19 16:51:35 -0500

    tutorial draft done

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
 var tabs = document.getElementsByClassName("js-tabs__tab");
 
+var contents = document.getElementsByClassName("js-content");
+
 window.addEventListener("load", function() {
 
 	for (i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener("click", function(event) {
             changeActiveState(event.target);
+            goToTab(event.target);
         });
     };
 
@@ -18,6 +21,20 @@ window.addEventListener("load", function() {
 
         tab.classList.add("tabs__tab--active");
         tab.dataset.content;
+
+    };
+
+    // Matches content with appropriate tab. Hides content from previous tab and shows content from active tab.
+    //
+    // tab - Node representing tab that was clicked.
+    function goToTab(tab) {
+        var content = document.querySelector("[data-content-for=" + tab.dataset.content + "]");
+
+        for (var n = contents.length - 1; n >= 0; n--) {
+            contents[n].classList.remove("content--active");
+        }
+
+        content.classList.add("content--active");	
 
     };
 
