0d46bb1de3ef913d167fe2617f5fadc2d9026844
Nikki <nicleemart@gmail.com>
2016-10-19 13:55:42 -0500

    working on tutorial

--- script2.js
+++ script2.js
@@ -1,15 +1,15 @@
+var tabs = document.getElementsByClassName("js-tabs");
+
+var active = document.getElementsByClassName("tabs__tab--active");
+
+var contents = document.getElementsByClassName("js-content");
+
 window.addEventListener("load", function() {
-
-    var tabs = document.getElementsByClassName("js-tabs");
-
-    var active = document.getElementsByClassName("tabs__tab--active");
-
-    var contents = document.getElementsByClassName("js-content");
 
     for (i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener("click", function(event) {
-            changeActiveState(event.target)
-            // goToTab();
+            changeActiveState(event.target);
+            goToTab(event.target);
         });
     };
 
@@ -20,17 +20,18 @@ window.addEventListener("load", function() {
 
         tab.classList.add("tabs__tab--active");
         tab.dataset.content;
+
+    };
+
+    function goToTab(tab) {
         var content = document.querySelector("[data-content-for=" + tab.dataset.content + "]");
 
         for (var n = contents.length - 1; n >= 0; n--) {
             contents[n].classList.remove("js-content--active");
         }
 
-        content.classList.add("js-content--active");
-    };
+        content.classList.add("js-content--active");	
 
-    // function goToTab() {	
-    //     }
-    // };
+    };
 
 });
